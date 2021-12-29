be880b4de09e2174c11adeacef78f5b68e01f2cb
Nick Reynolds <nick.reynolds@discordapp.com>
2021-12-29 14:25:24 -0800

    Header component

---
+++ components/heading.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Head from "next/head";
+
+const Heading = () => {
+  return (
+    <Head>
+      <title>Catnip</title>
+      <meta name="description" content="Catnip for your cat (and for you)" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/paw_favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/paw_favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/paw_favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/paw_favicon/site.webmanifest" />
+    </Head>
+  );
+};
+
+export default Heading;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Reloader from "../components/reloader"
-import TimeConsts from '../utils/timeConsts'
+import styles from "../styles/Home.module.css";
+import Reloader from "../components/reloader";
+import TimeConsts from "../utils/timeConsts";
 
 export default function Home() {
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Catnip</title>
-        <meta name="description" content="Catnip for your cat (and for you)" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/paw_favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/paw_favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/paw_favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/paw_favicon/site.webmanifest" />
-
-      </Head>
+      <Heading />
 
       <main className={styles.main}>
-          { /* TODO make this every day or so, maybe check if build is updated first */}
-          <Reloader durationMs={TimeConsts.MinuteInMs * 2}/>
+        {/* TODO make this every day or so, maybe check if build is updated first */}
+        <Reloader durationMs={TimeConsts.MinuteInMs * 2} />
 
-          <video autoPlay loop muted className={styles.video_container}>
-            <source src="/catnip_mice.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <video autoPlay loop muted className={styles.video_container}>
+          <source src="/catnip_mice.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </main>
     </div>
-  )
+  );
 }
