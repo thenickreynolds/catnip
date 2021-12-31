9c540694c6acf89a090cad537b6fb48cbd8e3efc
Nick Reynolds <nick.reynolds@discordapp.com>
2021-12-31 11:42:40 -0800

    fullscreen video again

--- components/video.jsx
+++ components/video.jsx
@@ -1,10 +1,11 @@
 import Videos from "../utils/videos";
+import styles from "../styles/Home.module.css";
 
 const Video = ({ videoName }) => {
   const vid = Videos.fromName(videoName);
 
   return (
-    <video autoPlay loop muted>
+    <video autoPlay loop muted className={styles.video_container}>
       <source src={vid.path} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
