1cbb24d8e5dbff9822b5ac7491495cd5831d25bf
Nick Reynolds <nick.reynolds@discordapp.com>
2021-12-31 14:39:52 -0800

    Reorder api again

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -2,13 +2,13 @@ import { MongoClient } from "mongodb";
 import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function settings(req, res) {
+  const session = await getSession(req, res);
+  const userId = session.user.sub;
+
+  const uri = process.env.MONGO_DB_URI;
+  const client = new MongoClient(uri);
+
   try {
-    const session = await getSession(req, res);
-    const userId = session.user.sub;
-
-    const uri = process.env.MONGO_DB_URI;
-    const client = new MongoClient(uri);
-
     // Connect the client to the server
     await client.connect();
 
